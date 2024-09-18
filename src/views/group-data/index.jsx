1f19f7a9417b043a7489a31537af1a5936cee60a
tcandra24 <tcandra007@gmail.com>
2024-09-18 10:26:52 +0700

    Rev add text box on scanner for general purpose

--- src/views/group-data/index.jsx
+++ src/views/group-data/index.jsx
@@ -12,40 +12,24 @@ const Index = () => {
   const { baseUrl, groupEndPoint, groupDetailEndPoint, token } = useAppState();
 
   const getData = async () => {
-    if (!baseUrl) {
-      toast.error("Base URL Belum terisi");
+    if (baseUrl && groupEndPoint && groupDetailEndPoint) {
+      try {
+        const apiService = api(baseUrl);
+        apiService.defaults.headers.common["Authorization"] = token;
 
-      return;
-    }
+        const { data } = await apiService.get(groupEndPoint);
 
-    if (!groupEndPoint) {
-      toast.error("End Point Belum terisi");
+        if (!data.success) {
+          throw new Error(data.message);
+        }
 
-      return;
-    }
-
-    if (!groupDetailEndPoint) {
-      toast.error("End Point Belum terisi");
-
-      return;
-    }
-
-    try {
-      const apiService = api(baseUrl);
-      apiService.defaults.headers.common["Authorization"] = token;
-
-      const { data } = await apiService.get(groupEndPoint);
-
-      if (!data.success) {
-        throw new Error(data.message);
+        dispatchData({
+          type: "GET_GROUP",
+          payload: data.data,
+        });
+      } catch (error) {
+        toast.error(error.response.data.message);
       }
-
-      dispatchData({
-        type: "GET_GROUP",
-        payload: data.data,
-      });
-    } catch (error) {
-      toast.error(error.response.data.message);
     }
   };
 
@@ -55,8 +39,32 @@ const Index = () => {
 
   return (
     <DefaultLayout>
-      <div className="flex flex-row flex-wrap gap-5">
+      <div className="flex flex-row flex-wrap gap-5 justify-center">
+        {stateData.groups && stateData.groups.length === 0 && (
+          <div className="lg:w-1/4 w-full">
+            <div
+              class="flex items-center p-4 mb-4 text-sm text-blue-800 rounded-lg bg-blue-50"
+              role="alert"
+            >
+              <svg
+                class="flex-shrink-0 inline w-4 h-4 me-3"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+              >
+                <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
+              </svg>
+              <span class="sr-only">Info</span>
+              <div>
+                <span class="font-medium">Info!</span> Group is Empty
+              </div>
+            </div>
+          </div>
+        )}
+
         {stateData.groups &&
+          stateData.groups.length > 0 &&
           stateData.groups.map((group) => (
             <div
               className="lg:w-1/4 w-full rounded overflow-hidden shadow-lg"
--- src/views/home/index.jsx
+++ src/views/home/index.jsx
@@ -11,6 +11,8 @@ const Home = () => {
   const [isPause, setIsPause] = useState(false);
   const [cameras, setCameras] = useState([]);
   const [selectedCamera, setSelectedCamera] = useState("");
+  const [resultScan, setResultScan] = useState("");
+
   const { baseUrl, scanEndPoint, token } = useAppState();
 
   const getDevices = async () => {
@@ -26,6 +28,9 @@ const Home = () => {
   };
 
   const handleCameraChange = (event) => {
+    if (cameras.length === 0) {
+      return;
+    }
     setSelectedCamera(event.target.value);
   };
 
@@ -33,34 +38,33 @@ const Home = () => {
     const [readerValue] = result;
     const value = readerValue.rawValue;
 
-    if (!baseUrl) {
-      toast.error("Base URL Belum terisi");
+    setResultScan(value);
 
-      return;
-    }
+    if (baseUrl && scanEndPoint) {
+      try {
+        const apiService = api(baseUrl);
+        apiService.defaults.headers.common["Authorization"] = token;
 
-    if (!scanEndPoint) {
-      toast.error("End Point Belum terisi");
+        const { data } = await apiService.post(scanEndPoint, {
+          token: value,
+        });
 
-      return;
-    }
+        if (!data.success) {
+          throw new Error(data.message);
+        }
 
-    try {
-      const apiService = api(baseUrl);
-      apiService.defaults.headers.common["Authorization"] = token;
-
-      const { data } = await apiService.post(scanEndPoint, {
-        token: value,
-      });
-
-      if (!data.success) {
-        throw new Error(data.message);
+        toast.success(data.message);
+      } catch (error) {
+        toast.error(error.response.data.message);
       }
-
-      toast.success(data.message);
-    } catch (error) {
-      toast.error(error.response.data.message);
     }
+  };
+
+  const isUrl = (value) => {
+    const urlRegex =
+      /^(?:(?:https?|ftp):\/\/)?(?:www\.)?[a-z0-9-]+(?:\.[a-z0-9-]+)+[^\s]*$/i;
+
+    return urlRegex.test(value);
   };
 
   useEffect(() => {
@@ -71,47 +75,72 @@ const Home = () => {
     <>
       <DefaultLayout>
         <div className="w-full h-full flex justify-center flex-col items-center bg-cover gap-2">
-          {cameras && (
-            <div className="relative flex w-full">
-              <div className="max-w-sm mx-auto">
-                <label
-                  htmlFor="cameras"
-                  className="block mb-2 text-sm font-medium text-gray-900"
-                >
-                  Select camera
-                </label>
-                <select
-                  id="cameras"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-indigo-500 focus:border-indigo-500 block w-full p-2.5"
-                  onChange={(e) => handleCameraChange(e)}
-                  value={selectedCamera}
-                >
-                  <option>Choose a camera</option>
-                  {cameras.map((camera) => (
-                    <option key={camera.deviceId} value={camera.deviceId}>
-                      {camera.label}
-                    </option>
-                  ))}
-                </select>
+          <div className="relative flex gap-2">
+            {cameras && (
+              <div className="w-3/4">
+                <div className="max-w-sm mx-auto">
+                  <select
+                    id="cameras"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-indigo-500 focus:border-indigo-500 block w-full p-2.5"
+                    onChange={(e) => handleCameraChange(e)}
+                    value={selectedCamera}
+                  >
+                    <option value={""}>Choose a camera</option>
+                    {cameras.map((camera) => (
+                      <option key={camera.deviceId} value={camera.deviceId}>
+                        {camera.label}
+                      </option>
+                    ))}
+                  </select>
+                </div>
               </div>
+            )}
+            <div className="w-1/4">
+              <button
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                onClick={() => setIsPause(!isPause)}
+              >
+                {isPause ? "Scan" : "Pause"}
+              </button>
             </div>
-          )}
-          <div className="relative border-8 overflow-hidden border-gray-600 bg-gray-60 rounded-3xl flex flex-col w-96 h-96 justify-center items-center bg-no-repeat bg-cover shadow-2xl">
-            <Scanner
-              paused={isPause}
-              scanDelay={2000}
-              className="absolute top-0 left-0 bg-black opacity-60 w-full h-full"
-              onScan={onScan}
-              constraints={{ video: { deviceId: selectedCamera } }}
-            />
           </div>
-          <div className="relative flex ">
-            <button
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-              onClick={() => setIsPause(!isPause)}
-            >
-              {isPause ? "Scan" : "Pause"}
-            </button>
+          <div className="w-auto">
+            <div className="relative border-8 overflow-hidden border-gray-600 bg-gray-60 rounded-3xl flex flex-col w-96 h-96 justify-center items-center bg-no-repeat bg-cover shadow-2xl">
+              {selectedCamera && (
+                <Scanner
+                  paused={isPause}
+                  scanDelay={2000}
+                  className="absolute top-0 left-0 bg-black opacity-60 w-full h-full"
+                  onScan={onScan}
+                  onError={(error) => console.error(error)}
+                  constraints={{
+                    video: {
+                      deviceId: selectedCamera,
+                    },
+                  }}
+                />
+              )}
+            </div>
+          </div>
+          {isUrl(resultScan) ? (
+            <div className="relative flex my-3">
+              <a href={resultScan} target="_blank">
+                <span class="bg-blue-100 text-blue-800 text-xl font-medium me-2 px-2.5 py-0.5 rounded ">
+                  {resultScan}
+                </span>
+              </a>
+            </div>
+          ) : (
+            ""
+          )}
+          <div className="relative flex my-3">
+            <textarea
+              className="w-96 border-2 rounded border-gray-300"
+              cols="30"
+              rows="5"
+              value={resultScan}
+              disabled={true}
+            ></textarea>
           </div>
         </div>
       </DefaultLayout>
