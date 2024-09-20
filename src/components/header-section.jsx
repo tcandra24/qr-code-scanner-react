995a4d2bf8ded8508f2caab57ed78dfa423e913a
tcandra24 <tcandra007@gmail.com>
2024-09-20 11:11:47 +0700

    Rev. Delete Camera Option

---
+++ src/components/header-section.jsx
@@ -0,0 +1,79 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+const HeaderSection = () => {
+  const [isNavOpen, setNavOpen] = useState(false);
+
+  return (
+    <nav className="bg-indigo-500 border-gray-200">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <Link
+          to="/"
+          className="flex items-center space-x-3 rtl:space-x-reverse"
+        >
+          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+            QrCode Scanner
+          </span>
+        </Link>
+        <button
+          onClick={() => setNavOpen(!isNavOpen)}
+          data-collapse-toggle="navbar-default"
+          type="button"
+          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-white rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 "
+          aria-controls="navbar-default"
+          aria-expanded="false"
+        >
+          <span className="sr-only">Open main menu</span>
+          <svg
+            className="w-5 h-5"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 17 14"
+          >
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M1 1h15M1 7h15M1 13h15"
+            />
+          </svg>
+        </button>
+        <div
+          className={`${!isNavOpen ? "hidden" : ""} w-full md:block md:w-auto`}
+          id="navbar-default"
+        >
+          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 bg-indigo-500 border-indigo-500">
+            <li>
+              <Link
+                to="/"
+                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0  md:p-0 dark:text-white  dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+              >
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/groups"
+                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0  md:p-0 dark:text-white  dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+              >
+                Groups
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/setting"
+                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0  md:p-0 dark:text-white  dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+              >
+                Setting
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default HeaderSection;
--- src/views/home/index.jsx
+++ src/views/home/index.jsx
@@ -9,27 +9,9 @@ import api from "../../services/api";
 
 const Home = () => {
   const [isPause, setIsPause] = useState(false);
-  const [cameras, setCameras] = useState([]);
-  const [selectedCamera, setSelectedCamera] = useState("");
   const [resultScan, setResultScan] = useState("");
 
   const { base_url, scan_end_point, token } = useAppState();
-
-  const getDevices = async () => {
-    const devices = await navigator.mediaDevices.enumerateDevices();
-    const videoDevices = devices.filter(
-      (device) => device.kind === "videoinput"
-    );
-    setCameras(videoDevices);
-
-    // if (videoDevices.length > 0) {
-    //   setSelectedCamera(videoDevices[0].deviceId);
-    // }
-  };
-
-  const handleCameraChange = async (event) => {
-    setSelectedCamera(event.target.value);
-  };
 
   const onScan = async (result) => {
     const [readerValue] = result;
@@ -64,34 +46,22 @@ const Home = () => {
     return urlRegex.test(value);
   };
 
-  useEffect(() => {
-    getDevices();
-  }, []);
-
   return (
     <>
       <DefaultLayout>
         <div className="w-full h-full flex justify-center flex-col items-center bg-cover gap-2">
+          <div className="w-auto">
+            <div className="relative border-8 overflow-hidden border-gray-600 bg-gray-60 rounded-3xl flex flex-col w-96 h-96 justify-center items-center bg-no-repeat bg-cover shadow-2xl">
+              <Scanner
+                paused={isPause}
+                scanDelay={2000}
+                className="absolute top-0 left-0 bg-black opacity-60 w-full h-full"
+                onScan={onScan}
+                onError={(error) => console.error(error)}
+              />
+            </div>
+          </div>
           <div className="relative flex gap-2">
-            {cameras && (
-              <div className="w-3/4">
-                <div className="max-w-sm mx-auto">
-                  <select
-                    id="cameras"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-indigo-500 focus:border-indigo-500 block w-full p-2.5"
-                    onChange={(e) => handleCameraChange(e)}
-                    value={selectedCamera}
-                  >
-                    <option value={""}>Choose a camera</option>
-                    {cameras.map((camera, index) => (
-                      <option key={index} value={camera.deviceId}>
-                        {camera.label}
-                      </option>
-                    ))}
-                  </select>
-                </div>
-              </div>
-            )}
             <div className="w-1/4">
               <button
                 className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
@@ -99,25 +69,6 @@ const Home = () => {
               >
                 {isPause ? "Scan" : "Pause"}
               </button>
-            </div>
-          </div>
-          <div className="w-auto">
-            <div className="relative border-8 overflow-hidden border-gray-600 bg-gray-60 rounded-3xl flex flex-col w-96 h-96 justify-center items-center bg-no-repeat bg-cover shadow-2xl">
-              {selectedCamera && (
-                <Scanner
-                  key={selectedCamera}
-                  paused={isPause}
-                  scanDelay={2000}
-                  className="absolute top-0 left-0 bg-black opacity-60 w-full h-full"
-                  onScan={onScan}
-                  onError={(error) => console.error(error)}
-                  constraints={{
-                    video: {
-                      deviceId: selectedCamera,
-                    },
-                  }}
-                />
-              )}
             </div>
           </div>
           {isUrl(resultScan) ? (
