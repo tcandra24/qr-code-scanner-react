a380f0b6d5c790d8da548e74195fda02939b14dd
tcandra24 <tcandra007@gmail.com>
2024-12-24 07:59:25 +0700

    Add Download Btn Qr Code

--- src/views/generator/index.jsx
+++ src/views/generator/index.jsx
@@ -1,9 +1,19 @@
 import DefaultLayout from "../../layouts/Default";
-import { useState } from "react";
-import { QRCodeSVG } from "qrcode.react";
+import { useRef, useState } from "react";
+import { QRCodeCanvas } from "qrcode.react";
 
 const Generator = () => {
   const [text, isText] = useState("");
+  const qrRef = useRef();
+
+  const downloadQrCode = () => {
+    const canvas = qrRef.current.querySelector("canvas");
+    const url = canvas.toDataURL("image/png");
+    const link = document.createElement("a");
+    link.href = url;
+    link.download = "qrcode.png";
+    link.click();
+  };
 
   return (
     <DefaultLayout>
@@ -16,13 +26,23 @@ const Generator = () => {
             onChange={(e) => isText(e.target.value)}
           ></textarea>
         </div>
-        <div className="w-auto">
+        <div className="w-auto" ref={qrRef}>
           {text && (
-            <QRCodeSVG
-              className="relative border-8 overflow-hidden border-gray-600 bg-gray-60 rounded-3xl flex flex-col w-96 h-96 justify-center items-center bg-no-repeat bg-cover shadow-2xl p-5"
+            <QRCodeCanvas
+              className="relative border-8 overflow-hidden border-gray-600 bg-gray-60 rounded-3xl flex flex-col w-96 h-96 justify-center items-center bg-no-repeat bg-cover shadow-2xl"
               value={text}
+              size={400}
+              marginSize={1}
             />
           )}
+        </div>
+        <div className="w-auto">
+          <button
+            onClick={downloadQrCode}
+            className="bg-blue-500 text-white px-4 py-2 rounded"
+          >
+            Download QR Code
+          </button>
         </div>
       </div>
     </DefaultLayout>
